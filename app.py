0a1d9fb24ef20bfdd893cf75d9192ccbd6761f6d
hanumansitaram <hanuman@unv.rs>
2020-06-14 01:33:07 +0200

    thirdDay

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 # Flask application for managing tickets
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, Response
 
+import flask
+
+# Setting an app
 app = Flask(__name__)
-#
+
+
 @app.route('/')
 def hello():
     name = "ticketingSystem"
     return render_template('home.html', name=name)
     
 # Definicija rute za login
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if flask.request.method == 'GET':
+        return render_template('login.html')
+
+    email = flask.request.form['email']
+    if flask.request.form['password'] == users[email]['password']:
+        user = User() 
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'    
+
+
+
+        
+    
 
 
     
